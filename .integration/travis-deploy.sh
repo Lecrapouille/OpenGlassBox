2011e18905a9992761af037fa84d6e2ac559453a
Quentin Quadrat <lecrapouille@gmail.com>
2020-10-20 23:10:27 +0200

    Fix CI

--- .integration/travis-deploy.sh
+++ .integration/travis-deploy.sh
@@ -4,8 +4,8 @@
 
 ### Deploy the unit tests coverage report
 RAPPORT=build/rapport.info
-cd tests
-lcov --quiet --directory .. -c -o $RAPPORT
-lcov --quiet --remove $RAPPORT '/usr*' 'external/*' 'tests/*' -o $RAPPORT
-coveralls-lcov --source-encoding=ISO-8859-1 $RAPPORT
-cd ..
+(cd tests
+   lcov --quiet --directory .. -c -o $RAPPORT
+   lcov --quiet --remove $RAPPORT '/usr*' 'external/*' 'tests/*' -o $RAPPORT
+   coveralls-lcov --source-encoding=ISO-8859-1 $RAPPORT
+)
